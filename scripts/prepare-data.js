4b92382180c933f21e10e2946c96fb76cb4c9596
pauliustechin <pauliustechin@gmail.com>
2025-12-11 09:20:06 +0200

    upgrade load-data to prepare-data, create split-data util

---
+++ scripts/prepare-data.js
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import { splitData } from "../utils/split-data.js"
+
+"use strict";
+
+function loadData(){
+  const loadDataPromises = Promise.all([
+    // kad pasimokint, viena promise is kart su fs moduliu pasidariau, o kita susikuriau pats.
+    fs.promises.readFile(`../zaidejai.txt`, {encoding: 'utf-8'}),
+
+    new Promise((resolve) => {
+      const data = fs.readFileSync(`../taskai.txt`, 'utf-8')
+        resolve(data);
+      })
+  ]).catch((error) => {
+      console.error("Something went wrong while loading a file.");
+      console.error(error);
+  });
+  return loadDataPromises;
+}
+
+
+export function readData(){
+  return new Promise((resolve) => {
+    loadData()
+    .then(([rawPlayerInfo, rawScoreInfo]) => {
+    const playerInfo = rawPlayerInfo.split("\r\n");
+    const scoreInfo = rawScoreInfo.split("\r\n");
+    return [playerInfo, scoreInfo];
+    })
+    .then(([playerInfo, scoreInfo]) => {
+      const player = splitData(playerInfo);
+      const score = splitData(scoreInfo);
+      // kadangi resolve negali grazinti multiple values, abu array isidedu i viena array
+      resolve([player, score]);
+    })
+    .catch((error) => {
+      console.error("Something went wrong while reading a file.");
+      console.error(error);
+    });
+  })
+}
+
+
